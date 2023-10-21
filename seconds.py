d1782004b195ce02d2b93ca4df6ad8500e1df0d6
rafael-917854632 <130230198+rafael-917854632@users.noreply.github.com>
2023-10-21 14:46:46 +0300

    Add files via upload

---
+++ seconds.py
@@ -0,0 +1,15 @@
+# напиши модуль для реализации секундомера
+
+class Seconds(Label):
+
+    def __init__(self, total, **kwargs):
+        pass
+
+    def restart(self, total, **kwargs):
+        pass
+
+    def stat(self):
+        pass
+
+    def change(self, dt):
+        pass
